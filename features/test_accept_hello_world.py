70ea3780657b706514ac4c3649d90f6b500ef786
Dmytri Kleiner <dev@dmytri.to>
2025-04-20 13:45:19 +0200

    refactor: Rename target_url fixture to service

--- features/test_accept_hello_world.py
+++ features/test_accept_hello_world.py
@@ -1,11 +1,10 @@
 import httpx
 from typing import Callable
-from pytest import skip
 from pytest_bdd import scenarios, given, when, then
 
 scenarios("accept_hello_world.feature")
 
-@given("the hello world URL", target_fixture="target_url")
+@given("the hello world URL", target_fixture="service")
 def _(marked: Callable[[str], bool]) -> str:
     if marked('dev'):
         return "http://localhost:8080/"
@@ -15,8 +14,8 @@ def _(marked: Callable[[str], bool]) -> str:
         return "https://asym.me/tric;to=hello-world"
 
 @when("the user browses the hello world site")
-def _(target_url: str, context) -> None:
-    response = httpx.get(target_url)
+def _(service: str, context) -> None:
+    response = httpx.get(service)
     context["response"] = response
 
 @then("the response status code should be 200")
