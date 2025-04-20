5feed609b66bfa19df6f680ae468fbf4303c59d8
Dmytri Kleiner <dev@dmytri.to>
2025-04-20 13:24:00 +0200

    test: implement hello world acceptance tests

--- features/accept_hello_world.feature
+++ features/accept_hello_world.feature
@@ -2,6 +2,6 @@ Feature: Acceptance tests for Hello World site
 
   @accept
   Scenario: Hello World home site
-    When the user accesses browser the hello world site
+    When the user browses the hello world site
     Then the response status code should be 200
     And the response should contain "hello world"
--- features/test_accept_hello_world.py
+++ features/test_accept_hello_world.py
@@ -1,20 +1,20 @@
-from pytest import skip
+import httpx
 from pytest_bdd import scenarios, given, when, then
 
 scenarios("accept_hello_world.feature")
 
-@given("the target container is running")
-def _() -> None:
-    skip("not implemented")
+@given("the hello world URL")
+def _(marked: Callable[[str], bool], target_fixture("target_url")) -> None:
 
-@when("the user accesses browser the hello world site")
-def _() -> None:
-    skip("not implemented")
+@when("the user browses the hello world site")
+def _(context) -> None:
+    response = httpx.get("http://example.com/hello-world")
+    context["response"] = response
 
 @then("the response status code should be 200")
-def _() -> None:
-    skip("not implemented")
+def _(context) -> None:
+    assert context["response"].status_code == 200
 
 @then('the response should contain "hello world"')
-def _() -> None:
-    skip("not implemented")
+def _(context) -> None:
+    assert "hello world" in context["response"].text
